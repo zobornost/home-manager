989f091b1d5a8741a9102b52c80ad42804fa8f05
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2025-05-16 18:21:06 +0100

    gnome stuff

--- home.nix
+++ home.nix
@@ -1,20 +1,23 @@
 { config, pkgs, inputs, ... }:
 {
-  home.username = "oz";
-  home.homeDirectory = "/home/oz";
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-  home.packages = with pkgs; [
-    blesh
-    emacs
-    fd
-    godot_4
-    ghc
-    haskellPackages.haskell-language-server
-  ];
-  home.sessionVariables = {
-    EDITOR = "code";
-    TERMINAL = "bash";
+  home = {
+    username = "oz";
+    homeDirectory = "/home/oz";
+    stateVersion = "24.11"; # Please read the comment before changing.
+    packages = with pkgs; [
+      blesh
+      emacs
+      fd
+      godot_4
+      ghc
+    ];
+    sessionVariables = {
+      EDITOR = "code";
+      TERMINAL = "bash";
+    };
   };
+  catppuccin.flavor = "mocha";
+  nixpkgs.config.allowUnfree = true;
   programs = {
     bash = {
       enable = true;
@@ -40,7 +43,9 @@
       userName = "Oz Browning";
       userEmail = "56755170+ozmodeuz@users.noreply.github.com";
     };
-    home-manager.enable = true;
+    home-manager = {
+      enable = true;
+    };
     starship = {
       enable = true;
       enableBashIntegration = true;
@@ -51,5 +56,10 @@
         };
       };
     };
+  };
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
 }
