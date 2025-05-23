10850cc48b9a12caa0681171a781b46b3878a2a9
Zoe Browning <56755170+ozmodeuz@users.noreply.github.com>
2025-05-23 13:34:46 +0100

    dev env changes

--- windsurf.nix
+++ windsurf.nix
@@ -1,38 +1,111 @@
 { pkgs, ... }:
 
 let
-  windsurfEnv = pkgs.buildFHSEnv {
+  windsurfRustEnv = pkgs.buildFHSEnv {
     name = "windsurf-rust";
-    # Include required packages
-    targetPkgs = pkgs: with pkgs; [
-      bash windsurf rustup
-      gcc binutils gnumake 
-      pkg-config openssl.dev gtk3.dev
-    ];
-    
+    targetPkgs =
+      pkgs: with pkgs; [
+        atk.dev
+        bash
+        binutils
+        cairo.dev
+        cargo
+        gdk-pixbuf.dev
+        gcc
+        gnumake
+        harfbuzz.dev
+        glib.dev
+        gobject-introspection.dev
+        graphene.dev
+        gtk3.dev
+        gtk4.dev
+        libadwaita.dev
+        openssl.dev
+        pango.dev
+        pkg-config
+        rustc
+        rustfmt
+        rustPlatform.rustLibSrc
+        vulkan-headers
+        vulkan-loader.dev
+        vulkan-tools
+        windsurf
+      ];
+
     runScript = ''
       bash -c '
+        export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
         export HOME=/home/oz/.windsurf-rust
-        export PATH="$PATH:${pkgs.rustup}/bin"
+        exec windsurf
+      '
+    '';
+  };
+  windsurfValaEnv = pkgs.buildFHSEnv {
+    name = "windsurf-vala";
+    targetPkgs =
+      pkgs: with pkgs; [
+        atk.dev
+        bash
+        binutils
+        cairo.dev
+        fontconfig.dev
+        freetype.dev
+        gdk-pixbuf.dev
+        gcc
+        glib.dev
+        gtk4.dev
+        gobject-introspection.dev
+        graphene.dev
+        gtk3.dev
+        harfbuzz.dev
+        libadwaita.dev
+        meson
+        ninja
+        pango.dev
+        pkg-config
+        python3
+        vala
+        vulkan-headers
+        vulkan-loader.dev
+        vulkan-tools
+        windsurf
+      ];
+    runScript = ''
+      bash -c '
+        export HOME=/home/oz/.windsurf-vala
         exec windsurf
       '
     '';
   };
 in
-
 {
-  home.packages = [ windsurfEnv ];
-  
+  home.packages = [
+    windsurfRustEnv
+    windsurfValaEnv
+  ];
+
   # Add a desktop entry for easily launching Windsurf in container
   home.file.".local/share/applications/windsurf-rust.desktop".text = ''
     [Desktop Entry]
     Name=Windsurf (Rust)
     Comment=Windsurf with isolated Rust environment
-    Exec=${windsurfEnv}/bin/windsurf-rust
+    Exec=${windsurfRustEnv}/bin/windsurf-rust
     Terminal=false
     Type=Application
     Categories=Development;
     Icon=windsurf
     StartupWMClass=windsurf-rust
+  '';
+
+  home.file.".local/share/applications/windsurf-vala.desktop".text = ''
+    [Desktop Entry]
+    Name=Windsurf (Vala)
+    Comment=Windsurf with isolated Vala environment
+    Exec=${windsurfValaEnv}/bin/windsurf-vala
+    Terminal=false
+    Type=Application
+    Categories=Development;
+    Icon=windsurf
+    StartupWMClass=windsurf-vala
   '';
 }
